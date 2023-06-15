d4acfc3af3fbb6fee90b851a7bb65fc80741e86b
redmojo <donaldchoi5180@gmail.com>
2023-06-15 12:49:14 +0800

    update FirstLoginModal

--- frontend/src/Components/FirstLoginModal.js
+++ frontend/src/Components/FirstLoginModal.js
@@ -1,27 +1,47 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, ListGroup } from "react-bootstrap";
 
 const FirstLoginModal = ({ show, onHide }) => {
   return (
     <Modal show={show} onHide={onHide}>
-    <Modal.Header closeButton>
-      <Modal.Title>Welcome to the Web Application!</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <p>Here is a quick guide on how to use the application:</p>
-      <p>1. The navigation bar is located at the top of the page.</p>
-      <p>2. You can click on the different links in the navigation bar to navigate to different sections of the application.</p>
-      <p>3. The "Truck(On-site)" link will take you to the on-site truck section.</p>
-      <p>4. The "Truck(Off-site)" link will take you to the off-site truck section.</p>
-      <p>5. The "Contact" link will take you to the contact section.</p>
-      {/* Add any other instructions you want */}
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="primary" onClick={onHide}>
-        Got it!
-      </Button>
-    </Modal.Footer>
-  </Modal>
+      <Modal.Header closeButton>
+        <Modal.Title>Welcome to the Web Application!</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Here is a quick guide on how to use the application:</p>
+        <ListGroup variant="flush">
+          <ListGroup.Item variant="primary">
+            Step 1: Navigation
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Click on the navigation bar located at the top of the page.
+          </ListGroup.Item>
+          <ListGroup.Item variant="success">
+            Step 2: On-site Trucks
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Click on "Truck(On-site)" in the navigation bar to view on-site trucks.
+          </ListGroup.Item>
+          <ListGroup.Item variant="info">
+            Step 3: Off-site Trucks
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Click on "Truck(Off-site)" in the navigation bar to view off-site trucks.
+          </ListGroup.Item>
+          <ListGroup.Item variant="warning">
+            Step 4: Contact
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Click on "Contact" in the navigation bar to access the contact section.
+          </ListGroup.Item>
+        </ListGroup>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={onHide}>
+          Got it!
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
